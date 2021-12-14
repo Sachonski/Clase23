e32e41025fe4165506550058ed8769d823568d9c
Alejandro Telatnik <alejandrojtelatnik@gmail.com>
2021-12-13 23:27:00 -0300

    arreglado

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,16 +9,16 @@ const productsController = require('../controllers/productsController');
 
 // ************ Multer ****************
 const date = Date.now();
-const storage = multer.diskStorage({
+var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images/products/');
+      cb(null, 'public/img/uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, date +"_"+file.originalname);
+     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
-});
-const upload = multer({ storage: storage });
-
+  }) 
+  var upload = multer({ storage: storage })
+  
 // *************** ROUTES *****************	
 
 /*** GET ALL PRODUCTS ***/ 
